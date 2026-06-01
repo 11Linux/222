e93832f7e7b97de73884f6f43c3ef9edd6fa9154
lxt1128 <2492268675@qq.com>
2026-06-01 15:15:36 +0800

    Delete bin directory

--- bin/review_done.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-DATA_DIR="$(dirname "$0")/../data"
-REVIEW_LOG="$DATA_DIR/logs/review.log"
-id=$1
-[[ -z $id ]] && { echo "用法: $0 <错题id>"; exit 1; }
-echo "[$(date '+%F %T')] 已完成复习: $id" >> "$REVIEW_LOG"
-echo "✅ 已打卡"
